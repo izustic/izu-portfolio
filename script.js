91ad49c1cdd16e1be77f96abccf74e461348c5b5
Izuchukwu Obi <izuchukwuobi997@gmail.com>
2023-01-26 20:50:58 +0100

    Added auto typing feature for hero text

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+const textEl = document.getElementById('text')
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
+}
+
+const text = "Hi, my name is Izu Obi. I'm a Web Developer. I'm super chill and a great guy to work with."
+let idx = 1
+let speed = 100
+
+writeText() 
+
+function writeText(){
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx>text.length){
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+
 }
