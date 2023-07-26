db82fb545491eaf88fbc083eeebe72e9f2162305
Izuchukwu Obi <izuchukwuobi997@gmail.com>
2023-07-26 06:30:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ menu.onclick = () => {
     navbar.classList.toggle('open');
 }
 
-const text = "Hi, I'm Izu Obi. I'm a full stack developer who focuses on writing clean, elegant, and efficient code."
+const text = "Hi, my name is Izu Obi. I'm a full stack developer who focuses on writing clean, elegant, and efficient code."
 let idx = 1
 let speed = 100
 
